9d8f479a2c86fb6cc77009abfaf6a675b1465fd0
igv7 <46227257+igv7@users.noreply.github.com>
2019-09-18 21:32:12 +0300

    Adding @bean configuration

--- CouponSystemSpringProj/src/main/java/com/igor/CouponSystemSpringProj/CouponSystemSpringProjApplication.java
+++ CouponSystemSpringProj/src/main/java/com/igor/CouponSystemSpringProj/CouponSystemSpringProjApplication.java
@@ -1,9 +1,14 @@
 package com.igor.CouponSystemSpringProj;
 
+import java.util.HashMap;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.igor.CouponSystemSpringProj.rest.ClientSession;
 
 @SpringBootApplication//(scanBasePackages="com.igor.CouponSystemSpringProj")
 @EnableJpaRepositories//
@@ -13,6 +18,11 @@ public class CouponSystemSpringProjApplication {
 //		ConfigurableApplicationContext context = SpringApplication.run(CouponSystemSpringProjApplication.class, args);
 		SpringApplication.run(CouponSystemSpringProjApplication.class, args);
 		System.out.println("Welcome to Coupon System!");
+	}
+	
+	@Bean
+	public HashMap<String, ClientSession> getHashMap(){
+		return new HashMap<String, ClientSession>();
 	}
 
 }
