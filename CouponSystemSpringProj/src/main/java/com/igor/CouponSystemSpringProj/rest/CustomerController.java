0d1cf80ae4e14c716a9b1ba4dd2d5e3be597c8c0
igv7 <46227257+igv7@users.noreply.github.com>
2019-09-29 00:39:47 +0300

    Adding a code

--- CouponSystemSpringProj/src/main/java/com/igor/CouponSystemSpringProj/rest/CustomerController.java
+++ CouponSystemSpringProj/src/main/java/com/igor/CouponSystemSpringProj/rest/CustomerController.java
@@ -3,9 +3,17 @@ package com.igor.CouponSystemSpringProj.rest;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.igor.CouponSystemSpringProj.enums.CouponType;
+import com.igor.CouponSystemSpringProj.model.Coupon;
 import com.igor.CouponSystemSpringProj.service.CustomerService;
 
 @RestController
@@ -23,10 +31,69 @@ public class CustomerController {
 	private CustomerService customerService;
 	
 	
-	//Add Coupon (Purchase)
+	//Purchase Coupon
+	@PostMapping("/purchaseCoupon/{token}/{id}")
+	public ResponseEntity<String> purchaseCoupon(@RequestBody Coupon coupon, @PathVariable("token") String token, @PathVariable("id") long id) {
+		ClientSession clientSession = isActive(token);
+		if (clientSession != null) {
+			clientSession.setLastAccessed(System.currentTimeMillis());
+			try {
+				customerService.purchaseCoupon(id);
+				return new ResponseEntity<>("Success. Coupon purchased by customer ", HttpStatus.OK);
+			} catch (Exception e) {
+				return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+			}
+		} else {
+			return new ResponseEntity<>("Unauthorized. Session Timeout", HttpStatus.UNAUTHORIZED);
+		}
+	}
 	
 	//View All Purchased Coupons
+	@GetMapping("/viewAllPurchasedCoupons/{token}")
+	public ResponseEntity<?> getAllPurchasedCoupons(@PathVariable("token") String token) {
+		ClientSession clientSession = isActive(token);
+		if (clientSession != null) {
+			clientSession.setLastAccessed(System.currentTimeMillis());
+			try {
+				return new ResponseEntity<> (customerService.getAllPurchasedCoupons(), HttpStatus.OK);
+			} catch (Exception e) {
+				return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+			}
+		} else {
+			return new ResponseEntity<>("Unauthorized. Session Timeout", HttpStatus.UNAUTHORIZED);
+		}
+	}
+	
 	//View All Purchased Coupons By Type
+	@GetMapping("/viewAllPurchasedCouponsByType/{token}/{type}")
+	public ResponseEntity<?> getAllPurchasedCouponsByType(@PathVariable("token") String token, @PathVariable("type") CouponType type) {
+		ClientSession clientSession = isActive(token);
+		if (clientSession != null) {
+			clientSession.setLastAccessed(System.currentTimeMillis());
+			try {
+				return new ResponseEntity<> (customerService.getAllPurchasedCouponsByType(type), HttpStatus.OK);
+			} catch (Exception e) {
+				return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+			}
+		} else {
+			return new ResponseEntity<>("Unauthorized. Session Timeout", HttpStatus.UNAUTHORIZED);
+		}
+	}
+	
 	//View All Purchased Coupons By Price
+	@GetMapping("/viewAllPurchasedCouponsByPrice/{token}/{price}")
+	public ResponseEntity<?> getAllPurchasedCouponsByPrice(@PathVariable("token") String token, @PathVariable("price") double price) {
+		ClientSession clientSession = isActive(token);
+		if (clientSession != null) {
+			clientSession.setLastAccessed(System.currentTimeMillis());
+			try {
+				return new ResponseEntity<> (customerService.getAllPurchasedCouponsByPrice(price), HttpStatus.OK);
+			} catch (Exception e) {
+				return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+			}
+		} else {
+			return new ResponseEntity<>("Unauthorized. Session Timeout", HttpStatus.UNAUTHORIZED);
+		}
+	}
 
 }
