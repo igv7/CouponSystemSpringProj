27f412a6acdb309603d99e124479383a81f39dd9
igv7 <46227257+igv7@users.noreply.github.com>
2019-10-16 17:48:25 +0300

    Adding a code

--- CouponSystemSpringProj/src/main/java/com/igor/CouponSystemSpringProj/rest/LoginController.java
+++ CouponSystemSpringProj/src/main/java/com/igor/CouponSystemSpringProj/rest/LoginController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +19,7 @@ import com.igor.CouponSystemSpringProj.service.Facade;
 
 @RestController
 //@RequestMapping("/login")
+@CrossOrigin(origins = "http://localhost:4200")
 public class LoginController {
 	
 	@Autowired
